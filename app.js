2fc6fb1cd65bf7494692742c716c2e06673502b0
Oyinloye Peter <oyinloyepeter273@gmail.com>
2023-08-18 08:43:19 -0700

    text changed

--- app.js
+++ app.js
@@ -25,19 +25,22 @@ close.addEventListener('click', ()=> {
 })
 
 const sr = ScrollReveal({
-    loop: true,
     origin: 'top',      // Origin of the animation
-    distance: '20px',   // Distance to start animation (can be in pixels or percentage)
-    duration: 1000,     // Duration of the animation in milliseconds
+    distance: '100px',   // Distance to start animation (can be in pixels or percentage)
+    duration: 500,     // Duration of the animation in milliseconds
     delay: 200,         // Delay before the animation starts
-    easing: 'ease',     // Easing function for the animation
+    // easing: 'ease',     // Easing function for the animation
     mobile: false       // Whether to enable animations on mobile devices
+    // loop : true
   });
   
   const tex = document.querySelectorAll('.tex');
   const ship = document.querySelectorAll('.ship');
 sr.reveal('.tex', {origin: 'top'});
 sr.reveal('.ship', {origin: 'left'});
+sr.reveal('.Kindz', {origin: 'right'});
+sr.reveal('.Air', {origin: 'left'});
+sr.reveal('.Air3', {origin: 'left'});
 
 
 // ScrollReveal({
@@ -50,3 +53,9 @@ sr.reveal('.ship', {origin: 'left'});
 // ScrollReveal().reveal('.', {origin: 'left'});
 // ScrollReveal().reveal('.image', {origin: 'left'});
 // ScrollReveal().reveal('.headline', {origin: 'right'});
+
+// new Typed('.type', {
+//     strings : ['We are a full-service Ocean Freight forwarder licensed by the FMC operating as Ocean Transportation lntermediary("OTI"). We provide complete international freight forwarding and logistics services, including: ocean freight, air freight, customs clearance, loading and transloading, consolidatons, warehousing and distribution (including packing and crating), cargo insurance and other value added logistics services. Our Mission is to retain our leadership in providing affordable & efficient service. Visit us today. '],
+//     typeSpeed : 100,
+//     loop : false
+// })
